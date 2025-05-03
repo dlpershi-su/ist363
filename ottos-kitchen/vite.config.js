ef1a60cb2c5158a44afa82d784a564074450648d
dlpershi-su <dlpershi@syr.edu>
2025-05-03 13:09:48 -0400

    project3

--- ottos-kitchen/vite.config.js
+++ ottos-kitchen/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ist363/ottos-kitchen/', // ← Add this line
   plugins: [react()],
 })
